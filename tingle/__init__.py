4b7e5ff77eb1ea0ee2f9c22ceda136147c804539
tonyfast <tony.fast@gmail.com>
2020-10-04 13:32:59 -0400

    add extension

--- tingle/__init__.py
+++ tingle/__init__.py
@@ -7,3 +7,15 @@ from .loaders import *
 with Markdown(lazy=True):
     from . import yml, weave, parameterize
     from . import weave
+
+
+def load_ipython_extension(shell):
+    with Markdown():
+        from . import extension
+    extension.load_ipython_extension(shell)
+
+
+def unload_ipython_extension(shell):
+    with Markdown():
+        from . import extension
+    extension.unload_ipython_extension(shell)
