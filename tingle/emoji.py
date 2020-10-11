095dd69f59464bba78fbd1e4be58279ac1267b54
tonyfast <tony.fast@gmail.com>
2020-10-11 11:13:19 -0400

    add emoji and top_level_return transformers.

---
+++ tingle/emoji.py
@@ -0,0 +1,37 @@
+def demojize(lines, delimiters=('_', '_')):
+    str = ''.join(lines or [])
+    import tokenize
+    import emoji
+    import stringcase
+    tokens = []
+    try:
+        for token in list(tokenize.tokenize(
+                __import__('io').BytesIO(str.encode()).readline)):
+            if token.type == tokenize.ERRORTOKEN:
+                string = emoji.demojize(token.string, delimiters=delimiters
+                                        ).replace('-', '_').replace("’", "_")
+                if tokens and tokens[-1].type == tokenize.NAME:
+                    tokens[-1] = tokenize.TokenInfo(tokens[-1].type, tokens[-1].string +
+                                                    string, tokens[-1].start, tokens[-1].end, tokens[-1].line)
+                else:
+                    tokens.append(
+                        tokenize.TokenInfo(
+                            tokenize.NAME, string, token.start, token.end, token.line))
+            else:
+                tokens.append(token)
+        return tokenize.untokenize(tokens).decode().splitlines(True)
+    except BaseException:
+        ...
+    return ''.join(lines)
+
+
+def load_ipython_extension(shell):
+    if demojize not in shell.input_transformer_manager.cleanup_transforms:
+        shell.input_transformer_manager.cleanup_transforms.append(demojize)
+
+
+def unload_ipython_extension(shell):
+    if demojize not in shell.input_transformer_manager.cleanup_transforms:
+        shell.input_transformer_manager.cleanup_transforms.pop(
+            shell.input_transformer_manager.cleanup_transforms.index(demojize)
+        )
--- tingle/util.py
+++ tingle/util.py
@@ -1,6 +1,22 @@
 import contextlib
 
 
+def append_ast_transformers(shell, transformer):
+    if any(
+        x for x in shell.ast_transformers
+        if isinstance(x, transformer)
+    ):
+        return
+    shell.ast_transformers.append(transformer())
+
+
+def remove_ast_transformers(shell, transformer):
+    shell.ast_transformers = [
+        x for x in shell.ast_transformers
+        if not isinstance(x, transformer)
+    ]
+
+
 @contextlib.contextmanager
 def argv(list):
     import sys
