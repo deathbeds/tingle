237fad78fbf2f727812e40d7dc4b4bdca44ae451
tonyfast <tony.fast@gmail.com>
2020-11-05 22:08:25 -0500

    🤷

--- tingle/rst.py
+++ tingle/rst.py
@@ -1,14 +1,14 @@
 def rst2docutils(str):
+    """convert rst into docutils nodes"""
     import docutils.core
+    import docutils.parsers.rst
 
     settings = docutils.frontend.OptionParser(
         components=(docutils.parsers.rst.Parser,)
     ).get_default_values()
 
-    io = __import__('io').StringIO()
-
     reporter = docutils.utils.Reporter(
-        io,
+        __import__('io').StringIO(),
         settings.report_level,
         settings.halt_level,
         stream=settings.warning_stream,
@@ -16,7 +16,8 @@ def rst2docutils(str):
         encoding=settings.error_encoding,
         error_handler=settings.error_encoding_error_handler)
 
-    document = docutils.nodes.document(settings, reporter, source=io)
+    document = docutils.nodes.document(
+        settings, reporter, source=reporter.source)
 
     docutils.parsers.rst.Parser().parse(str, document)
     return document
